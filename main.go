67447a0aecc7d1b3902ef00708129879a77b7dec
BayajidAlam <bayajidalam2001@gmail.com>
2025-09-24 01:19:40 +0600

    if else switch practiced

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
-package main;
+package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello World!")
+func main() {
+	a := 2
+	if a >= 18 {
+		fmt.Println("Get married!")
+	} else {
+		fmt.Println("Can't get married!")
+	}
+
+	day := 3
+
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	default:
+		fmt.Println("Other day")
+	}
 }
