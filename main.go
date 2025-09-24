efba4fcd645c0f3ca98dca9bc0798f5db5bbe945
BayajidAlam <bayajidalam2001@gmail.com>
2025-09-25 01:23:04 +0600

    Func-with-return-and-data-types

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import "fmt"
 
-func sum(num1 int, num2 int) {
+func sum(num1 int, num2 int) int {
 	sum := num1 + num2
-	fmt.Println(sum)
+	return sum
 }
+
+func getNumbers(num1 int, num2 int) (int, int) {
+	sum := num1 + num2
+	mul := num1 * num2
+
+	return sum, mul
+}
+
 
 func main() {
 	a := 10
 	b := 20
-	sum(a, b)
+	// fmt.Println(sum(a, b))
+	fmt.Println(getNumbers(a, b))
 }
