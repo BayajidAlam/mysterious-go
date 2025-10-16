e60549bba0dbe6382a472984e088ba52302da778
BayajidAlam <bayajidalam2001@gmail.com>
2025-10-17 04:05:49 +0600

    Implemented advanced middleware

--- middleware/preflight.go
+++ middleware/preflight.go
@@ -2,8 +2,12 @@ package middleware
 
 import "net/http"
 
-func Beauty(next http.Handler) http.Handler {
+func Preflight(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(200)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
